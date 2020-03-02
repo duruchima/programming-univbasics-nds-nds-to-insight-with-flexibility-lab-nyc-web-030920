8156976c46334ab2a19b3622ca4bfe63e1bb056e
Duncan Uruchima <duruchima1@gmail.com>
2020-03-02 17:10:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,7 +32,7 @@ end
 
 
 # Your code after this point
-pp movies_collection
+
 def movies_with_director_key(name, movies_collection)
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
@@ -48,6 +48,7 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  pp movies_collection
 end
 
 
