2bce03f45c77e345dbc1e4280f343d3a9bbe2862
Duncan Uruchima <duruchima1@gmail.com>
2020-03-02 18:23:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -71,6 +71,7 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  pp collection
   total{}
   index=0
   movie_data = collection[index]
